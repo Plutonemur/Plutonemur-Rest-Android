3a73a77d9782007d49502af4bba40528ba912a9c
Han Cao <1787873794@qq.com>
2019-12-23 22:21:57 +0800

    Initial commit

---
+++ rest/src/main/java/com/plutonem/rest/RestClient.java
@@ -0,0 +1,157 @@
+package com.plutonem.rest;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Map;
+
+public class RestClient {
+    public static final String TAG = "PlutonemREST";
+    public static enum REST_CLIENT_VERSIONS {V0, V1, V1_1, V1_2, V1_3}
+    public static final String PARAMS_ENCODING = "UTF-8";
+
+    protected static final String REST_API_ENDPOINT_URL_V0 = "http://192.168.2.136:8080/";
+    protected static final String REST_API_ENDPOINT_URL_V1 = "http://192.168.2.136:8080/rest/v1/";
+    protected static final String REST_API_ENDPOINT_URL_V1_1 = "http://192.168.2.136:8080/rest/v1.1/";
+    protected static final String REST_API_ENDPOINT_URL_V1_2 = "http://192.168.2.136:8080/rest/v1.2/";
+    protected static final String REST_API_ENDPOINT_URL_V1_3 = "http://192.168.2.136:8080/rest/v1.3/";
+
+    private RequestQueue mQueue;
+    private String mAccessToken;
+    private String mUserAgent;
+    private String mRestApiEndpointURL;
+    private RestRequest.OnAuthFailedListener mOnAuthFailedListener;
+
+    public RestClient(RequestQueue queue) {
+        this(queue, REST_CLIENT_VERSIONS.V1);
+    }
+
+    public RestClient(RequestQueue queue, REST_CLIENT_VERSIONS version) {
+        mQueue = queue;
+
+        if (version == REST_CLIENT_VERSIONS.V1_3) {
+            mRestApiEndpointURL = REST_API_ENDPOINT_URL_V1_3;
+        } else if (version == REST_CLIENT_VERSIONS.V1_2) {
+            mRestApiEndpointURL = REST_API_ENDPOINT_URL_V1_2;
+        } else if (version == REST_CLIENT_VERSIONS.V1_1) {
+            mRestApiEndpointURL = REST_API_ENDPOINT_URL_V1_1;
+        } else if (version == REST_CLIENT_VERSIONS.V0) {
+            mRestApiEndpointURL = REST_API_ENDPOINT_URL_V0;
+        } else {
+            // Fallback to version 1
+            mRestApiEndpointURL = REST_API_ENDPOINT_URL_V1;
+        }
+    }
+
+    public RestClient(RequestQueue queue, String token, String endpointURL) {
+        this(queue);
+        mAccessToken = token;
+        mRestApiEndpointURL = endpointURL;
+    }
+
+    public RestRequest get(String path, Response.Listener<JSONObject> listener, Response.ErrorListener errorListener) {
+        return makeRequest(Request.Method.GET, getAbsoluteURL(path), null, listener, errorListener);
+    }
+
+    public RestRequest post(String path, Map<String, String> body, Response.Listener<JSONObject> listener,
+                            Response.ErrorListener errorListener) {
+        return makeRequest(Request.Method.POST, getAbsoluteURL(path), body, listener, errorListener);
+    }
+
+    public RestRequest makeRequest(int method, String url, Map<String, String> params, Response.Listener<JSONObject> listener,
+                                   Response.ErrorListener errorListener) {
+        RestRequest request = new RestRequest(method, url, params, listener, errorListener);
+        if (mOnAuthFailedListener != null) {
+            request.setOnAuthFailedListener(mOnAuthFailedListener);
+        }
+        request.setUserAgent(mUserAgent);
+        request.setAccessToken(mAccessToken);
+        return request;
+    }
+
+    public JsonRestRequest makeRequest(String url, JSONObject params, Response.Listener<JSONObject> listener,
+                                       Response.ErrorListener errorListener) {
+        JsonRestRequest request = new JsonRestRequest(url, params, listener, errorListener);
+        if (mOnAuthFailedListener != null) {
+            request.setOnAuthFailedListener(mOnAuthFailedListener);
+        }
+        request.setUserAgent(mUserAgent);
+        request.setAccessToken(mAccessToken);
+        return request;
+    }
+
+    public RestRequest send(RestRequest request) {
+        // Volley send the request
+        mQueue.add(request);
+        return request;
+    }
+
+    public void setOnAuthFailedListener(RestRequest.OnAuthFailedListener onAuthFailedListener) {
+        mOnAuthFailedListener = onAuthFailedListener;
+    }
+
+    public String getAbsoluteURL(String url) {
+        // if it already starts with our endpoint, let it pass through
+        if (url.indexOf(mRestApiEndpointURL) == 0) {
+            return url;
+        }
+        // if it has a leading slash, remove it
+        if (url.indexOf("/") == 0) {
+            url = url.substring(1);
+        }
+        // prepend the endpoint
+        return String.format("%s%s", mRestApiEndpointURL, url);
+    }
+
+    public String getAbsoluteURL(String path, Map<String, String> params) {
+        String url = getAbsoluteURL(path);
+        if (params != null) {
+            // build a query string
+            StringBuilder query = new StringBuilder();
+            try {
+                for (Map.Entry<String, String> entry : params.entrySet()) {
+                    query.append(URLEncoder.encode(entry.getKey(), PARAMS_ENCODING));
+                    query.append("=");
+                    query.append(URLEncoder.encode(entry.getValue(), PARAMS_ENCODING));
+                    query.append("&");
+                }
+                if (query.length() > 0) {
+                    query.deleteCharAt(query.length() - 1);
+                }
+            } catch (UnsupportedEncodingException uee) {
+                throw new RuntimeException("Encoding not supported: " + PARAMS_ENCODING, uee);
+            }
+
+            if (url.contains("?")) {
+                url = String.format("%s&%s", new Object[]{url, query});
+            } else {
+                url = String.format("%s?%s", new Object[]{url, query});
+            }
+        }
+        return url;
+    }
+
+    //Sets the User-Agent header to be sent with each future request.
+    public void setUserAgent(String userAgent) {
+        mUserAgent = userAgent;
+    }
+
+    // Sets the auth token to be used in the request header
+    public void setAccessToken(String token) {
+        mAccessToken = token;
+    }
+
+    public boolean isAuthenticated() {
+        return mAccessToken != null;
+    }
+
+    public String getEndpointURL() {
+        return mRestApiEndpointURL;
+    }
+}
+
---
+++ rest/src/main/java/com/plutonem/rest/RestRequest.java
@@ -0,0 +1,162 @@
+package com.plutonem.rest;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RestRequest extends Request<JSONObject> {
+    public static final String USER_AGENT_HEADER = "User-Agent";
+    public static final String REST_AUTHORIZATION_HEADER = "Authorization";
+    public static final String REST_AUTHORIZATION_FORMAT = "Bearer %s";
+    public static final String ORIGINAL_RESPONSE = "originalResponse";
+
+    private static OnAuthFailedListener mOnAuthFailedListener;
+
+    public interface Listener extends Response.Listener<JSONObject> {
+    } //This is just a shortcut for Response.Listener<JSONObject>
+    public interface ErrorListener extends Response.ErrorListener {
+    } //This is just a shortcut for Response.ErrorListener
+
+    public interface OnAuthFailedListener {
+        void onAuthFailed();
+    }
+
+    private final com.android.volley.Response.Listener<JSONObject> mListener;
+    private final Map<String, String> mParams;
+    private final Map<String, String> mHeaders = new HashMap<String, String>(2);
+
+    public RestRequest(int method, String url, Map<String, String> params,
+                       com.android.volley.Response.Listener<JSONObject> listener,
+                       com.android.volley.Response.ErrorListener errorListener) {
+        super(method, url, errorListener);
+        mParams = params;
+        mListener = listener;
+    }
+
+    public void removeAccessToken() {
+        setAccessToken(null);
+    }
+
+    public void setAccessToken(String token) {
+        if (token == null) {
+            mHeaders.remove(REST_AUTHORIZATION_HEADER);
+        } else {
+            mHeaders.put(REST_AUTHORIZATION_HEADER, String.format(REST_AUTHORIZATION_FORMAT, token));
+        }
+    }
+
+    public void setUserAgent(String userAgent) {
+        mHeaders.put(USER_AGENT_HEADER, userAgent);
+    }
+
+    public void setOnAuthFailedListener(OnAuthFailedListener onAuthFailedListener) {
+        mOnAuthFailedListener = onAuthFailedListener;
+    }
+
+    @Override
+    public Map<String, String> getHeaders() {
+        return mHeaders;
+    }
+
+    @Override
+    protected void deliverResponse(JSONObject response) {
+        if (mListener != null) {
+            mListener.onResponse(response);
+        }
+    }
+
+    @Override
+    protected Map<String, String> getParams() {
+        return mParams;
+    }
+
+    @Override
+    public void deliverError(VolleyError error) {
+        super.deliverError(error);
+
+        // Fire OnAuthFailedListener if we receive an invalid token error
+        if (error.networkResponse != null && error.networkResponse.statusCode >= 400 && mOnAuthFailedListener != null) {
+            String jsonString;
+            try {
+                jsonString = new String(error.networkResponse.data, HttpHeaderParser.parseCharset(error.networkResponse.headers));
+            } catch (UnsupportedEncodingException e) {
+                jsonString = "";
+            }
+
+            JSONObject responseObject;
+            try {
+                responseObject = new JSONObject(jsonString);
+            } catch (JSONException e) {
+                responseObject = new JSONObject();
+            }
+
+            String restError = responseObject.optString("error", "");
+            if (restError.equals("authorization_required") || restError.equals("invalid_token")) {
+                mOnAuthFailedListener.onAuthFailed();
+            }
+        }
+    }
+
+    @Override
+    protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+        try {
+            String jsonString = new String(response.data, HttpHeaderParser.parseCharset(response.headers));
+
+            JSONObject jsonObject;
+            try {
+                jsonObject = jsonObjectFromResponse(jsonString);
+            } catch (JSONException jsonException) {
+                try {
+                    jsonObject = jsonArrayObjectFromResponse(jsonString);
+                } catch (JSONException jsonArrayException) {
+                    try {
+                        jsonObject = jsonBooleanObjectFromResponse(jsonString);
+                    } catch (JSONException jsonBooleanException) {
+                        return Response.error(new ParseError(jsonBooleanException));
+                    }
+                }
+            }
+
+            return Response.success(jsonObject, HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            return Response.error(new ParseError(e));
+        }
+    }
+
+    protected JSONObject jsonObjectFromResponse(String jsonString) throws JSONException {
+        return new JSONObject(jsonString);
+    }
+
+    protected JSONObject jsonArrayObjectFromResponse(String jsonString) throws JSONException {
+        JSONArray responseArray = new JSONArray(jsonString);
+        JSONObject wrapper = new JSONObject();
+        wrapper.put(ORIGINAL_RESPONSE, responseArray);
+
+        return wrapper;
+    }
+
+    protected JSONObject jsonBooleanObjectFromResponse(String jsonString) throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+
+        if (jsonString.equals(Boolean.TRUE.toString())) {
+            jsonObject.put(ORIGINAL_RESPONSE, true);
+            return jsonObject;
+        } else if (jsonString.equals(Boolean.FALSE.toString())) {
+            jsonObject.put(ORIGINAL_RESPONSE, false);
+            return jsonObject;
+        } else {
+            throw new JSONException("Not a valid JSON response: " + jsonString);
+        }
+    }
+}
